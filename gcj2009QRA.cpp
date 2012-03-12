a79f56718f335f83af39ea91170ee673a50ef87a
sato <st@step.skr.jp>
2012-03-12 20:42:35 +0900

    GCJ 2009 Qualification Round A - refactoring
    space to tab

--- gcj2009QRA.cpp
+++ gcj2009QRA.cpp
@@ -4,20 +4,20 @@
 
 static void get_extxt( std::vector<std::string>& v )
 {
-    std::string str;
-    std::cin >> str;
-    const int len = str.length();
-    for (int i=0; i<len; ++i) {
-        int base = i;
-        int subs = 1;
-        if (str[i] == '(') {
-            const int b = str.find( ')', i+1 );
-            subs = b-i-1;
-            base = i+1;
-            i = b;
-        }
-        v.push_back(str.substr(base, subs));
-    }
+	std::string str;
+	std::cin >> str;
+	const int len = str.length();
+	for (int i=0; i<len; ++i) {
+		int base = i;
+		int subs = 1;
+		if (str[i] == '(') {
+			const int b = str.find( ')', i+1 );
+			subs = b-i-1;
+			base = i+1;
+			i = b;
+		}
+		v.push_back(str.substr(base, subs));
+	}
 }
 
 static bool check_match( const std::string& txt, const std::vector<std::string>& extxt )
@@ -36,8 +36,8 @@ static bool check_match( const std::string& txt, const std::vector<std::string>&
 
 static int count_match( const std::vector<std::string>& txt_list )
 {
-    std::vector<std::string> extxt;
-    get_extxt(extxt);
+	std::vector<std::string> extxt;
+	get_extxt(extxt);
 	int n = 0;
 	for (auto& v: txt_list) {
 		if (check_match(v, extxt)) {
@@ -49,13 +49,13 @@ static int count_match( const std::vector<std::string>& txt_list )
 
 int main () {
 	int L,D,N;
-    std::cin >> L >> D >> N;
-    std::vector<std::string> txt_list(D);
+	std::cin >> L >> D >> N;
+	std::vector<std::string> txt_list(D);
 	for (int i=0; i<D; ++i) {
-        std::cin >> txt_list[i];
-    }
-	for (int i=1; i<=N; ++i) {
-        std::cout << "Case #" << i << ": " << count_match(txt_list) << "\n";
+		std::cin >> txt_list[i];
 	}
-    return 0;
+	for (int i=1; i<=N; ++i) {
+		std::cout << "Case #" << i << ": " << count_match(txt_list) << "\n";
+	}
+	return 0;
 }
