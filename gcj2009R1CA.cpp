74187f587e42a003fed7eaeba210bcab2a2fcaab
sato <st@step.skr.jp>
2012-03-12 21:13:38 +0900

    GCJ 2009 Round 1C A - refactoring

--- gcj2009R1CA.cpp
+++ gcj2009R1CA.cpp
@@ -1,68 +1,30 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
-using namespace std;
-
-static int get_num( vector<int>& v )
+static unsigned long long solve()
 {
-	string str;
-	getline( cin, str );
-	const int len = str.length();
-	int n=1;
-	int number=0;
-	for (int i=0; i < len; ++i) {
-		if ( str[i] == ' ' ) {
-			v.push_back( number );
-			number = 0;
-			++n;
-			continue;
-		}
-		number = number*10+(str[i]-'0');
-	}
-	v.push_back( number );
-//	return n;
-	return number;
-}
-
-static int change( char c )
-{
-	if ('0'<= c && c <= '9') {
-		return c-'0';
-	}
-	else {
-		return c-'a'+10;
-	}
-
-}
-
-static unsigned long long calc( const string& str )
-{
+	std::string str;
+	std::cin >> str;
 	const int len = str.length();
 	int base = 0;
-	int use[36];
-	int num[36];
-	vector<int> v;
-	for (int i=0; i<36; ++i) {
-		use[i] = -1;
-		num[i] = -1;
-	}
-	for (int i=0; i<len; ++i) {
-		int k = change( str[i] );
-		if (-1 == use[k]) {
-			use[k] = i;
+	std::map<char,int> num;
+	for (auto const&k: str) {
+		if (0==num.count(k)) {
+			int n;
 			if (base > 1) {
-				num[k] = base;
+				n = base;
 			}
 			else if( 1 == base ) {
-				num[k] = 0;
+				n = 0;
 			}
 			else {
-				num[k] = 1;
+				n = 1;
 			}
+			num[k] = n;
 			++base;
 		}
-		v.push_back( k );
 	}
 	if (1 == base) {
 		base = 2;
@@ -70,7 +32,7 @@ static unsigned long long calc( const string& str )
 	unsigned long long unit = 1;
 	unsigned long long sum = 0;
 	for (int i=len-1; i>=0; --i) {
-		int k = v[i];
+		auto const& k = str[i];
 		sum += num[k] * unit;
 		unit *= base;
 	}
@@ -78,12 +40,10 @@ static unsigned long long calc( const string& str )
 }
 
 int main () {
-	vector<int> v;
-	const int n = get_num( v );
-	for (int i=0; i<n; ++i) {
-		string str;
-		getline( cin, str );
-		cout << "Case #" << i+1 << ": " << calc( str ) << "\n";
+	int T;
+	std::cin >> T;
+	for (int i=1; i<=T; ++i) {
+		std::cout << "Case #" << i << ": " << solve() << "\n";
 	}
-    return 0;
+	return 0;
 }
