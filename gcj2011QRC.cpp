55eef736983d073cc4a8bcd34a11303f5680581a
sato <st@step.skr.jp>
2012-03-14 23:47:04 +0900

    GCJ 2011 Qualification Round C - refactoring

--- gcj2011QRC.cpp
+++ gcj2011QRC.cpp
@@ -1,74 +1,30 @@
 #include <iostream>
 #include <string>
-#include <queue>
-#include <set>
 #include <sstream>
-
-typedef unsigned long v_type;
-typedef std::set<v_type> v_list_type;
-class state
-{
-public:
-    v_type s_sum;
-    v_type s_sum_b;
-    v_type p_sum_b;
-    v_list_type value_list;
-    bool operator<(const state& rhs) const { return this->s_sum < rhs.s_sum; }
-};
+#include <limits>
+#include <algorithm>
 
 static std::string solve () {
     int N;
     std::cin >> N;
-    v_type s_value;
-    std::set<v_list_type> visited;
-    std::priority_queue<state> Q;
-    {
-        state s;
-        s.s_sum = 0;
-        s.s_sum_b = 0;
-        s.p_sum_b = 0;
-        for (int i=0; i<N; ++i) {
-            v_type t;
-            std::cin >> t;
-            s.value_list.insert(t);
-            s.s_sum += t;
-            s.s_sum_b ^= t;
-        }
-        if (0 != s.s_sum_b) {
-            return "NO";
-        }
-        s_value = s.s_sum;
-        Q.push(s);
+    int sum = 0;
+    int sum_x = 0;
+    int min = std::numeric_limits<int>::max();
+    for (int i=0; i<N; ++i) {
+        int t;
+        std::cin >> t;
+        sum += t;
+        sum_x ^= t;
+        min = std::min(min,t);
     }
-    while (!Q.empty()) {
-        const state v = Q.top();
-        Q.pop();
-        if (visited.count(v.value_list)) {
-            continue;
-        }
-        visited.insert(v.value_list);
-        if (v.s_sum_b == v.p_sum_b && s_value != v.s_sum) {
-            std::stringstream ss;
-            ss << v.s_sum;
-            return ss.str();
-        }
-        typedef v_list_type::const_iterator Iter;
-        Iter end = v.value_list.end();
-        state v2 = v;
-        for (Iter it=v.value_list.begin(); end!=it; ++it) {
-            const v_type t = *it;
-            v2.s_sum -= t;
-            v2.s_sum_b ^= t;
-            v2.p_sum_b ^= t;
-            v2.value_list.erase(t);
-            Q.push(v2);
-            v2.s_sum += t;
-            v2.s_sum_b ^= t;
-            v2.p_sum_b ^= t;
-            v2.value_list.insert(t);
-        }
+    if (0!=sum_x) {
+        return "NO";
     }
-    return "NO";
+    else {
+        std::stringstream ss;
+        ss << (sum-min);
+        return ss.str();
+    }
 }
 
 int main ()
