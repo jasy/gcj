db007ec23b880e03786f887de5f517c99f0b9617
sato <st@step.skr.jp>
2012-03-15 21:22:13 +0900

    GCJ 2009 Round 1A A - refactoring

--- gcj2009R1AA.cpp
+++ gcj2009R1AA.cpp
@@ -6,20 +6,20 @@
 typedef unsigned long base_type;
 static int memo[11][20000000] = {};
 static int isHappy( const base_type no, const int base ) {
-    if (1==no) {
-        return 1;
-    }
-    if (memo[base][no]) {
-        return memo[base][no];
-    }
-    base_type m=no;
-    base_type n=0;
-    while (m>0) {
-        const base_type t = m%base;
-        n += t*t;
-        m /= base;
-    }
-    memo[base][no] = -1;
+	if (1==no) {
+		return 1;
+	}
+	if (memo[base][no]) {
+		return memo[base][no];
+	}
+	base_type m=no;
+	base_type n=0;
+	while (m>0) {
+		const base_type t = m%base;
+		n += t*t;
+		m /= base;
+	}
+	memo[base][no] = -1;
 	return memo[base][no] = isHappy(n,base);
 }
 
@@ -32,7 +32,7 @@ static base_type solve() {
 	while (iss >> base) {
 		bases.push_back( base );
 	}
-    const int n = bases.size();
+	const int n = bases.size();
 	base_type no;
 	bool happy;
 	for(no=2; ;++no) {
