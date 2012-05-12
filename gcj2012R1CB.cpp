364c573f13df0efba953617b4e162c4bb0d33215
sato <st@step.skr.jp>
2012-05-12 11:45:08 +0900

    GCJ 2012 Round 1C B
    距離が与えられたときの理想(最短)の移動方法はブレーキなし。
    このときの到着時間が前の車より早いとき、差分だけ待てば良い。
    自身は等加速かつ前の車は区間内では等速なので、指定のポイント毎に理想の到着時間と前の車の到着時間の差分を算出して、待ち時間の最大値を求める。
    その値と理想での目標地点までの時間を足せばOK。

--- gcj2012R1CB.cpp
+++ gcj2012R1CB.cpp
@@ -3,54 +3,38 @@
 #include <vector>
 #include <cmath>
 
-static void solve (const double D, const std::vector<double>& t, const std::vector<double> x, double a)
-{
-    double mt = 0;
-    double mx = 0;
-    double mv = 0;
-    const int n = t.size();
-    for (int i=1; i<n; ++i) {
-        double ct = t[i];
-        double cx = x[i];
-        if (cx>D) {
-            ct = t[i-1] + (ct-t[i-1])*(D-x[i-1])/(cx-x[i-1]);
-            cx = D;
-        }
-        const double d = cx - mx;
-        const double dt = (-mv + sqrt(mv*mv+2*a*d))/a;
-        if (mt+dt >= ct) {
-            mt += dt;
-            mx += d;
-            mv += a*dt;
-        }
-        else {
-            mt = ct;
-            mx += d;
-            mv = (cx-x[i-1])/(ct-t[i-1]);
-        }
-        if (mx >= D) {
-            break;
-        }
-    }
-    std::cout << mt << "\n";
-}
-
 static void solve ()
 {
     double D;
     int N, A;
     std::cin >> D >> N >> A;
-    std::vector<double> t(N+1,0.0);
-    std::vector<double> x(N+1,0.0);
-    for (int i=1; i<=N; ++i) {
+    std::vector<double> t(N);
+    std::vector<double> x(N);
+    for (int i=0; i<N; ++i) {
         std::cin >> t[i] >> x[i];
     }
     std::vector<double> a(A);
     for (int i=0; i<A; ++i) {
         std::cin >> a[i];
     }
+    for (int i=0; i<N; ++i) {
+        if (x[i]>=D) {
+            if (0!=i)
+                t[i] = t[i-1] + (t[i]-t[i-1])*(D-x[i-1])/(x[i]-x[i-1]);
+            x[i] = D;
+            N = i+1;
+            break;
+        }
+    }
     for (int i=0; i<A; ++i) {
-        solve(D, t, x, a[i]);
+        const double ai = a[i];
+        double s = 0.0;
+        double ti = 0.0;
+        for (int i=0; i<N; ++i) {
+            ti = std::sqrt(2*x[i]/ai);
+            s = std::max(s, t[i]-ti);
+        }
+        std::cout << (s+ti) << "\n";
     }
 }
 
