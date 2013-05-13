e111670d09b4f9f0cacf538243c5d426c4db247c
sato <st@step.skr.jp>
2013-05-14 00:23:05 +0900

    GCJ 2013 Round 1C B
    n手使うと1〜nの合計値までの距離の1〜nまでの合計値が偶数ならx+y偶数の点、奇数ならx+y奇数の点すべてに移動できる。
    ※ 1〜3くらい試すと直感的にもわかると思う。
    すなわちX+Y<=sum(1〜n)かつ(X+Y)%2==sum(1〜n)%2を満たす最小のnを見つければ最小手数がわかる。
    1〜m手使うとその位置を中心にsum(1〜m)の範囲動けるのでn〜1の順に目標位置X,Yに近づく(X,Yの遠い方に近づく)ように方向を決定すれば
    OK。

--- gcj2013R1CB.d
+++ gcj2013R1CB.d
@@ -1,32 +1,29 @@
 #!/usr/bin/env dmd -O -run
 import std.stdio, std.string, std.conv;
 import std.algorithm;
+import std.math;
 
 auto solve()
 {
     auto XY = readln().split().map!(to!int);
     auto X = XY[0];
     auto Y = XY[1];
-    char[] r;
-    if(X!=0)
+    long n = 0;
     {
-        int a = X>0? X: -X;
-        int d = X>0? 1: -1;
-        foreach(i;0..a*2)
-        {
-            r ~= d>0? 'W': 'E';
-            d *= -1;
-        }
+        auto A = X.abs() + Y.abs();
+        long s=0;
+        while(s<A || A%2!=s%2)
+            ++n,s=n*(n+1)/2;
     }
-    if(Y!=0)
+    auto r = new char[n];
+    foreach_reverse(i;0..n)
     {
-        int a = Y>0? Y: -Y;
-        int d = Y>0? 1: -1;
-        foreach(i;0..a*2)
-        {
-            r ~= d>0? 'S': 'N';
-            d *= -1;
-        }
+        if(X.abs()>Y.abs())
+            if(X>0) X-=i+1,r[i]='E';
+            else    X+=i+1,r[i]='W';
+        else
+            if(Y>0) Y-=i+1,r[i]='N';
+            else    Y+=i+1,r[i]='S';
     }
     return r;
 }
