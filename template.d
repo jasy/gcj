a21e40d72e96b2fb9237f75da71b17090e546c52
sato <st@step.skr.jp>
2014-01-10 19:54:52 +0900

    D template
    ・定型の出力フォーマット

---
+++ template.d
@@ -0,0 +1,13 @@
+#!/usr/bin/env rdmd -O
+import std.stdio, std.string, std.conv;
+
+auto solve()
+{
+    return format("%.6f",0.0);
+}
+
+void main()
+{
+    foreach(i;0..readln().chomp().to!int())
+        writeln("Case #",i+1,": ",solve());
+}
