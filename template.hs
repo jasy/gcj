3119440268b0dd4306934d63368997f55d6b7174
sato <st@step.skr.jp>
2014-01-10 19:55:23 +0900

    Haskell template
    ・定型の出力フォーマット

---
+++ template.hs
@@ -0,0 +1,8 @@
+#!/usr/bin/env runghc
+import Text.Printf
+main = getContents >>= mapM_ putStrLn . zipWith caseno [1..] . solve . lines
+caseno i r = "Case #"++show i++": "++r
+solve (c:cs) = map (toString . answer) . take (read c) . analyze $ cs ::[String]
+analyze = repeat . const 0.0
+toString = printf "%.6f" ::Double->String
+answer = id
