62d2a7f14c9216fa3387540280ac2cbec0b3fc7b
sato <st@step.skr.jp>
2014-02-03 01:16:11 +0900

    GCJ 2009 Qualification Round A
    解析対象の入力文字列が正規表現そっくりなので、正規表現書式に変換してそのままチェック。

--- gcj2009QRA.cpp
+++ gcj2009QRA.cpp
@@ -1,61 +1,32 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
+#include <algorithm>
+#include <regex>
 
-static void get_extxt( std::vector<std::string>& v )
+static auto solve = [](const std::vector<std::string>& d)
 {
-	std::string str;
-	std::cin >> str;
-	const int len = str.length();
-	for (int i=0; i<len; ++i) {
-		int base = i;
-		int subs = 1;
-		if (str[i] == '(') {
-			const int b = str.find( ')', i+1 );
-			subs = b-i-1;
-			base = i+1;
-			i = b;
-		}
-		v.push_back(str.substr(base, subs));
-	}
-}
+    std::string w; std::cin >> w;
+    for(auto& v: w)
+        switch(v)
+        {
+        case '(': v='['; break;
+        case ')': v=']'; break;
+        default: break;
+        }
+    std::regex r(w);
+    int c=0;
+    for(auto& v: d)
+        if(std::regex_match(v,r))
+            ++c;
+    return c;
+};
 
-static bool check_match( const std::string& txt, const std::vector<std::string>& extxt )
+int main()
 {
-	bool match = true;
-	const int n = txt.length();
-	for (int i=0; i<n; ++i) {
-		if( std::string::npos == extxt[i].find(txt[i]) )
-		{
-			match = false;
-			break;
-		}
-	}
-	return match;
-}
-
-static int count_match( const std::vector<std::string>& txt_list )
-{
-	std::vector<std::string> extxt;
-	get_extxt(extxt);
-	int n = 0;
-	for (auto& v: txt_list) {
-		if (check_match(v, extxt)) {
-			++n;
-		}
-	}
-	return n;
-}
-
-int main () {
-	int L,D,N;
-	std::cin >> L >> D >> N;
-	std::vector<std::string> txt_list(D);
-	for (int i=0; i<D; ++i) {
-		std::cin >> txt_list[i];
-	}
-	for (int i=1; i<=N; ++i) {
-		std::cout << "Case #" << i << ": " << count_match(txt_list) << "\n";
-	}
-	return 0;
+    int L,D,N; std::cin >> L >> D >> N;
+    std::vector<std::string> d(D);
+    for(auto& v: d) std::cin >> v;
+    for(int i=1; i<=N; ++i)
+        std::cout << "Case #" << i << ": " << solve(d) << '\n';
 }
