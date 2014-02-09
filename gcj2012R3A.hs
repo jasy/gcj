5e4fedd584506d6f66febe039fdf31a1354a2b85
sato <st@step.skr.jp>
2014-02-09 14:43:18 +0900

    GCJ 2012 Round 3 A
    ※ コードを整理しただけ
    コスト計算してソートする問題。
    方向性としては以下を後回し。
    ・失敗確率の低いもの
    ・時間がかかるもの(smallは全部1)
    なのでコストとしては失敗確率/時間の大きなものからチョイス。
    辞書順最小の条件があるのでコストが同じなら先に出てくるものから選ぶのがポイント

--- gcj2012R3A.hs
+++ gcj2012R3A.hs
@@ -1,17 +1,9 @@
 #!/usr/bin/env runghc
-import Control.Monad
 import Data.List
-
-main = do
-  c <- liftM lines getContents
-  let ss = zipWith (\i r->"Case #"++show i++": "++r) [1..] $ solve c
-  mapM_ putStrLn ss
-
-solve (c:cs) = map u $ take (read c) $ analyze cs
-  where
-    u (_:l:p:[]) = foldr (\a b -> ' ' : show a ++ b) "" i
-      where
-        i = map ((n-) . snd) . reverse . sort . zip e . reverse $ take n [1..]
-        n = length e
-        e = (zipWith (/) p l)
-    analyze xs = map (map read . words) (take 3 xs) : analyze (drop 3 xs)
+import Data.List.Split
+main = getContents >>= mapM_ putStrLn . zipWith caseno [1..] . solve . lines
+caseno i r = "Case #"++show i++": "++r
+solve (c:cs) = map (toString . answer) . take (read c) . analyze $ cs ::[String]
+analyze = map (map (map read . words)) . chunksOf 3
+toString = unwords . map show
+answer [_,l,p] = map (negate . snd) . reverse . sort . zip (zipWith (/) p l) $ [0,-1..]
