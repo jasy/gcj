35a9691efdba5e9a8442b0bb6bcbb170de3beaa8
sato <st@step.skr.jp>
2014-02-18 20:02:01 +0900

    Haskell template
    並列処理するように変更。
    ただしrunghcではそうならないので
    ghc -threaded と実行時の +RTS -N2 が必要。数字はコア数に応じて変更する。

--- template.hs
+++ template.hs
@@ -1,8 +1,9 @@
 #!/usr/bin/env runghc
+import Control.Parallel.Strategies
 import Text.Printf
 main = getContents >>= mapM_ putStrLn . zipWith caseno [1..] . solve . lines
 caseno i r = "Case #"++show i++": "++r
-solve (c:cs) = map (toString . answer) . take (read c) . analyze $ cs ::[String]
+solve (c:cs) = parMap rpar (toString . answer) . take (read c) . analyze $ cs ::[String]
 analyze = repeat . const 0.0
 toString = printf "%.6f" ::Double->String
 answer = id
