3e2587d794b52d02073db9d5eacb03a6ef9ce46e
sato <st@step.skr.jp>
2014-02-24 22:53:30 +0900

    GCJ 2013 Qualification Round B
    長方形の辺から垂直に反対の辺まで同じ高さでしか刈れない。
    各行と列の最大値をチェックして
    全要素で行と列両方の最大値より小さい値だとNG
    ※ 型指定の書き方変えただけ

--- gcj2013QRB.hs
+++ gcj2013QRB.hs
@@ -3,7 +3,7 @@ import Data.List
 main = getContents >>= mapM_ putStrLn . zipWith caseno [1..] . solve . lines
 caseno i r = "Case #"++show i++": "++r
 solve (c:cs) = map (toString . answer) . take (read c) . analyze $ cs ::[String]
-analyze (c:cs) = map (map (read::String->Int) . words) a: analyze r
+analyze (c:cs) = map (map read . words) a: analyze r ::[[[Int]]]
   where (a,r) = splitAt (read . head . words $ c) cs
 toString a = if a then "YES" else "NO"
 answer a = and . zipWith f c $ a where
