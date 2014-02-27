2baa4aa4c0d5149c96115373d18eda84f1655276
sato <st@step.skr.jp>
2014-02-27 18:29:29 +0900

    GCJ 2013 Round 1C A
    最小文字数揃うところを探して、
    その前に揃ったものから1文字削ったところと最後までの組み合わせを足していく。
    ※ 毎回最初から最後までの組み合わせやると重複カウントしてしまう
    ※ 再帰を自前化して省メモリ化＆高速化するようにした

--- gcj2013R1CA.hs
+++ gcj2013R1CA.hs
@@ -4,9 +4,10 @@ caseno i r = "Case #"++show i++": "++r
 solve (c:cs) = map (toString . answer) . take (read c) . analyze $ cs ::[String]
 analyze = map ((\[l,n]->(l,read n::Int)) . words)
 toString = show
-answer (l,n) = fst . foldr cnt (0,(m,0)) . zip [0..] $ l where
+answer (l,n) = cnt (0,(-1,0)) . zip [0..] $ l where
   m = length l
-  cnt (i,c) (s,(b,cc))
-    | c `elem` "aeiou" = (s,(b,0))
-    | cc+1<n = (s,(b,cc+1))
-    | otherwise = (s+(i+1)*(b-i-n+1),(i+n-1,cc+1))
+  cnt (s,_) [] = s
+  cnt (s,(b,cc)) ((i,c):xs)
+    | c `elem` "aeiou" = cnt (s,(b,0)) xs
+    | cc+1<n = cnt (s,(b,cc+1)) xs
+    | otherwise = cnt (s+(i-n-b+1)*(m-i),(i-n+1,cc+1)) xs
