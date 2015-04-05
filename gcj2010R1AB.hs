934cf72f304eea6f8aa6f783eacfe57c64f3d80c
jasy <jasy@outlook.jp>
2015-04-06 01:03:07 +0900

    GCJ 2010 Round 1A B
    リストをVectorに置き換えて高速化によりLargeに対応

--- gcj2010R1AB.hs
+++ gcj2010R1AB.hs
@@ -1,25 +1,17 @@
 #!/usr/bin/env runghc
-import Control.Monad
-
-main = do
-  c <- liftM lines getContents
-  let ss = zipWith (\i r->"Case #"++show i++": "++r) [1..] $ solve c
-  mapM_ putStrLn ss
-
-solve (c:cs) = map u $ analyze cs
-  where
-    u [[d,i,m,n],a] = show $ f a (repeat 0)
-      where
-        f [] dp = minimum dp
-        f (a:as) dp = f as dpn
-          where
-            dpn = ins [min (del x) (mdf x)|x<-[0..255]]
-            ins dp = if m==0 then dp else [minimum (zipWith (\v c->c+((abs (x-v))+m-1)`div`m*i) [0..255] dp)|x<-[0..255]]
-            del x = dp!!x+d
-            mdf x = minimum $ map (+d) $ take (e-s+1) $ drop s dp 
-              where
-                d = abs (a-x)
-                s = max 0 (x-m)
-                e = min (x+m) 255
-    analyze [] = []
-    analyze xs = map (map read . words) (take 2 xs) : analyze (drop 2 xs)
+import Data.List
+import Data.List.Split
+import qualified Data.Vector.Unboxed as V
+main = mapM_ putStrLn . zipWith caseno [1..] . solve . lines =<< getContents
+caseno i r = "Case #"++show i++": "++r
+solve (c:cs) = map (toString . answer) . take (read c) . analyze $ cs ::[String]
+analyze = chunksOf 2 . map (map read . words)
+toString = show
+answer [[d,i,m,n],a] = V.minimum $ foldl' f (V.replicate 256 0) a where
+  f dp a = ins $ V.generate 256 (\x->min (del x) (mdf x)) where
+    ins dp = if m==0 then dp else V.generate 256 (\x->V.minimum (V.zipWith (\v c->c+((abs (x-v))+m-1)`div`m*i) (V.enumFromN 0 256) dp))
+    del x = dp V.! x + d
+    mdf x = (+d) $ V.minimum $ V.slice s (e-s+1) dp where
+      d = abs (a-x)
+      s = max 0 (x-m)
+      e = min (x+m) 255
