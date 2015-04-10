02405bb3078b6d9cee80ba4794e7e62f7afc0fbc
jasy <jasy@outlook.jp>
2015-04-11 07:59:17 +0900

    GCJ 2014 Qualification Round A
    Swift 1.2で書き直し。アルゴリズムはSet使う

--- gcj2014QRA.swift
+++ gcj2014QRA.swift
@@ -1,7 +1,7 @@
 #!/usr/bin/env swift
 import Foundation
 func getContens() -> String {
-    return NSString(data: NSFileHandle.fileHandleWithStandardInput().availableData, encoding:NSUTF8StringEncoding) ?? ""
+    return NSString(data: NSFileHandle.fileHandleWithStandardInput().availableData, encoding:NSUTF8StringEncoding)! as String
 }
 func strip(s: String, charset: NSCharacterSet) -> String {
     return s.stringByTrimmingCharactersInSet(charset)
@@ -13,23 +13,17 @@ func lines(s: String) -> [String] {
     return strip(s,NSCharacterSet.newlineCharacterSet()).componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet())
 }
 func getInts(s: String) -> [Int] {
-    return split(s,{$0==" "}).map({$0.toInt()!})
+    return split(s){$0==" "}.map({$0.toInt()!})
 }
-func solve(input: Slice<String>) -> String {
+func solve(input: ArraySlice<String>) -> String {
     let an = getInts(input[input[0].toInt()!+0])
     let bn = getInts(input[input[5].toInt()!+5])
-    var n = -1
-    for a in an {
-        for b in bn {
-            if a==b {
-                if n>=0 { return "Bad magician!" }
-                n = a
-                break
-            }
-        }
+    let s = Set(an).intersect(bn)
+    switch s.count {
+        case 0: return "Volunteer cheated!"
+        case 1: return Array(s)[0].description
+        default: return "Bad magician!"
     }
-    if n<0 { return "Volunteer cheated!" }
-    return n.description
 }
 let cs = lines(getContens())
 for t in 1...cs[0].toInt()! {
