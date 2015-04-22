a1e9ab3ff0903cebcbb995d1d81591cb8a5feeb7
jasy <jasy@outlook.jp>
2015-04-22 22:46:22 +0900

    Python template
    標準入力を組み込み関数に変更

--- template.py
+++ template.py
@@ -4,10 +4,9 @@ def solve(i):
     return "%.6f" % i
 
 if __name__=='__main__':
-    from sys import stdin
     def analyze(i):
         return 0.0
-    xs = map(analyze,range(int(stdin.readline())))
+    xs = map(analyze,range(int(raw_input())))
     from multiprocessing import Pool
     for i,v in enumerate(Pool().imap(solve,xs)):
         print("Case #"+str(i+1)+": "+str(v))
