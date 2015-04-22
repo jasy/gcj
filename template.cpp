993fbf50ec1256372c0b098df59365f67815d608
jasy <jasy@outlook.jp>
2015-04-22 22:41:49 +0900

    C++ template
    ・C++14向けに変更
    ・浮動小数点出力桁数設定

--- template.cpp
+++ template.cpp
@@ -1,7 +1,8 @@
-// clang++ -std=c++11 -stdlib=libc++ -Wall -Wextra -O3 -march=native
+// clang++ -std=c++14 -stdlib=libc++ -Wall -Wextra -O3 -march=native
 #include <iostream>
+#include <iomanip>
 
-static auto solve = []()
+static auto solve()
 {
     return 0.0;
 };
@@ -10,9 +11,8 @@ int main()
 {
     std::cin.tie(0);
     std::ios_base::sync_with_stdio(false);
-    std::cout << std::fixed;
-    int T;
-    std::cin >> T;
+    std::cout << std::fixed << std::setprecision(6);
+    int T; std::cin >> T;
     for (int i=1; i<=T; ++i)
         std::cout << "Case #" << i << ": " << solve() << '\n';
 }
