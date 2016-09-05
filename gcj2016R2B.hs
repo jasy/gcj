6fe381ea6c94d0ba65823aa7fd0bc28852992bb3
jasy <jasy@outlook.jp>
2016-09-05 09:20:06 -0700

    GCJ 2016 Round 2 B
    確率の極端に高いor低いものを選ぶと同点になる確率が高くなる(実験からなる経験的事実)。
    確率の低いi人と確率の高いK-i人を選び、その中から最も高いものが解。

--- gcj2016R2B.hs
+++ gcj2016R2B.hs
@@ -1,6 +1,6 @@
 #!/usr/bin/env runghc
 import Control.Parallel.Strategies
-import Data.Bits
+import Data.List
 import Data.List.Split
 import Text.Printf
 main = mapM_ putStrLn . zipWith caseno [1..] . solve . lines =<< getContents
@@ -9,6 +9,6 @@ solve (c:cs) = parMap rpar (toString . answer) . take (read c) . analyze $ cs ::
 analyze = map (\[nk,ps]->(map read nk,map read ps)) . chunksOf 2 . map words
 toString = printf "%.6f"::Double->String
 answer ([n,k],ps) = maximum $ map f a where
-  a = map (\x->map (testBit x) [0..n-1]) . filter ((==k).popCount) $ [0..(bit n-1::Int)]
-  f = (!!(k`div`2)) . foldr g [1.0] . map fst . filter snd . zip ps
+  a = let qs=sort ps in map (\i->take i qs++drop (n-k+i) qs) [0..k]
+  f = (!!(k`div`2)) . foldr g [1.0]
   g p qs = zipWith (+) (0.0:map (*p) qs) (map (*(1-p)) qs++[0.0])
