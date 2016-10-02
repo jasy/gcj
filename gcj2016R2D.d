d149b79818eea942e261aba021ce837629388ec9
jasy <jasy@outlook.jp>
2016-10-02 12:47:35 -0700

    GCJ 2016 Round 2 D
    最初に関連があるものをグループとみて、全グループの組み合わせから最小コストを出す動的計画法。
    人と機械が同一数で全員が全ての機械を操作できるグループを作ると要求を満たすことができる。
    2N(N人+N台)の関係から最初にできているグループを算出(ここではWarshall Floydしている。Union
    FindとかでもOK)。基準を満たさないグループを併合して基準達成に持っていく。
    最悪の場合全てが関連なしなので2Nのグループができる。2Nのうちどれを使うかでbit
    DP(動的計画法)したいところだが2N=50と計算量が大きすぎるので以下の省略を入れる。
    ・最初から人と機械が同一数のものは併合させる必要がないので確定させておく(2がなくなる)。
    ・どれとも関連がないもの(1人or1台だけ)は同一視する(1がなくなる)。
    これにより考慮すべきグループの最小数が3となる顧慮すべきグループ数の最大が2N/3=16とあまりとなる。
    使うグループの組み合わせ×1人グループの数×1台グループの数が動的計画法で求める組み合わせ数。
    算出時に上記使うグループの組み合わせのサブグループ全通りとその残り(その前に算出済みである)を使って最善値を求める。
    この時サブグループの数の平均が全体のグループ数の組み合わせの半分になる。
    ここまでで1人or1台が多いよりも考慮するグループ数が多い方が計算量が大きくなるのはわかるのでそのときの計算量を確認する。
    2^(2N/3*1.5)=2^N=2^25
    と十分間に合う計算量である。

--- gcj2016R2D.d
+++ gcj2016R2D.d
@@ -1,38 +1,55 @@
 #!/usr/bin/env rdmd -O
 import std.stdio, std.string, std.conv;
-import std.algorithm;
+import std.algorithm, std.array;
 import core.bitop;
 
 auto solve()
 {
-    uint tobin(string s){ uint b=0; foreach(c;s) b<<=1,b|=(c=='0'?0:1); return b; }
     immutable N = readln.chomp.to!int();
-    uint t = 0;
-    foreach(_;0..N) t<<=N, t|=tobin(readln.chomp());
-    int m=int.max;
-    foreach(b;0..1<<N*N) if((b&t)==t)
-    {
-        bool check()
-        {
-            auto a = new uint[N];
-            foreach(i,ref v;a) v=b>>i*N&(1<<N)-1;
-            sort(a);
-            uint u=0;
-            bool f=true;
-            for(int i=0; i<N;)
-            {
-                immutable v=a[i];
-                immutable n=_popcnt(v);
-                if(v==0||(v&u)||i+n>N) return false;
-                u|=v;
-                foreach(w;a[i+1..i+n]) if(v!=w) return false;
-                i+=n;
-            }
-            return u==(1<<N)-1;
-        }
-        if(check()) m=min(m,_popcnt(b^t));
+    alias ulong T;
+    assert(N<=T.sizeof*8/2);
+    int c=0;
+    auto d = new T[N*2];
+    foreach(i,ref v;d) v|=T(1)<<i;
+    foreach(i;0..N) foreach(j,b;readln()[0..N]) if(b!='0') {
+        d[i] |= T(1)<<j+N;
+        d[j+N] |= T(1)<<i;
+        --c;
     }
-    return m;
+    foreach(k;0..N*2) foreach(i;0..N*2) foreach(j;0..N*2)
+        d[i] |= (d[i]>>k & d[k]>>j & 1)<<j;
+    T u=0;
+    int[2] s; s[]=0;
+    int[2][] g;
+    int[2][] r; r~=[0,0];
+    foreach(i;0..N*2) if((u>>i&1)==0) {
+        immutable v=d[i];
+        u|=v;
+        immutable a=_popcnt(v&(1<<N)-1), b=_popcnt(v>>N);
+        immutable int[2] t=[a,b];
+        if(a==b) c+=a*b;
+        else if(a+b==1) s[]+=t[];
+        else {
+            g~=t;
+            r.reserve(r.length*2);
+            foreach(ref w;r[0..$]) ++r.length, r.back[]=w[]+t[];
+        }
+    }
+    auto dp = new int[][][](r.length,s[0]+1,s[1]+1);
+    immutable INF = int.max/2;
+    foreach(ref dpm;dp) foreach(ref dpi;dpm) dpi[]=INF;
+    dp[0][0][0]=0;
+    foreach(m,ref dpm;dp) foreach(int i,ref dpi;dpm) foreach(int j,ref v;dpi) {
+        if(i>0 && j>0) v=dp[m][i-1][j-1]+1;
+        for(auto m2=m; m2>0; m2=m2-1&m) {
+            immutable a=r[m2][0],b=r[m2][1];
+            immutable t=max(a,b);
+            immutable ri=i-(t-a), rj=j-(t-b);
+            if(ri<0 || rj<0) continue;
+            v=min(v,dp[m^m2][ri][rj]+t*t);
+        }
+    }
+    return c+dp[$-1][$-1][$-1];
 }
 
 void main()
