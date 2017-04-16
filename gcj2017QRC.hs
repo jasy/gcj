7cdd4657ece3e2bce5c71be2e15245fc6a40d9da
jasy <jasy@outlook.jp>
2017-04-16 16:44:36 -0700

    GCJ 2017 Qualification Round C
    最後の人の条件さえわかれば良いので、2つに分断する領域の最後となる領域のみ再帰的に計算する。
    現在の領域が奇数のとき、残りは同数に分かれ、人数を2つに割った多い方が最終になる。
    現在の領域が偶数のとき、残り人数が偶数であれば狭い方、残り人数が奇数であれば広い方が最終になる。

---
+++ gcj2017QRC.hs
@@ -0,0 +1,13 @@
+#!/usr/bin/env runghc
+import Control.Parallel.Strategies
+import Data.List
+main = getContents >>= mapM_ putStrLn . zipWith caseno [1..] . solve . lines
+caseno i r = "Case #"++show i++": "++r
+solve (c:cs) = parMap rpar (toString . answer) . take (read c) . analyze $ cs ::[String]
+analyze = map ((\[a,b]->(a,b)) . map read . words)
+toString = intercalate " " . map show
+answer (n,k)
+  |k==1 = [n`div`2,(n-1)`div`2]
+  |odd(n) = answer (n`div`2,k`div`2)
+  |odd(k) = answer (n`div`2-1,k`div`2)
+  |otherwise = answer (n`div`2,k`div`2)
