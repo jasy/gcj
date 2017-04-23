32937aff230300f42bce915167c23c3683a10b0c
jasy <jasy@outlook.jp>
2017-04-23 13:34:05 -0700

    D template
    記述量削減のため入力のwithによるスコープ変更追加

--- template.d
+++ template.d
@@ -4,18 +4,20 @@ import std.algorithm, std.array, std.range;
 import std.parallelism;
 
 struct IN {
+    real X;
 };
 
-void input(ref IN c) {
-}
+void input(ref IN c_) {with(c_){
+    X=0.0;
+}}
 
-auto solve(IN c) {
-    return format("%.6f",0.0);
-}
+auto solve(IN c_) {with(c_){
+    return format("%.6f",X);
+}}
 
 void main() {
     auto cs = new IN[readln.chomp.to!int()];
-    foreach(ref c;cs) input(c);
+    foreach(ref c_;cs) input(c_);
     foreach(i,a;taskPool.amap!solve(cs))
         writeln("Case #",i+1,": ",a);
 }
